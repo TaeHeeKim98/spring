ebd840810e302abe08024d0d2401d1aee72df662
TaeHeeKim <eksrnsdhkdwk@gmail.com>
2022-10-02 02:43:12 +0900

    no message

--- spring_2/webprj/src/main/java/com/newlecture/web/controller/HomeController.java
+++ spring_2/webprj/src/main/java/com/newlecture/web/controller/HomeController.java
@@ -1,7 +1,13 @@
 package com.newlecture.web.controller;
 
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.http.HttpServletResponse;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 @RequestMapping("/")
